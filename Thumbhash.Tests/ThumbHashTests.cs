e02eec1715090f9e7137013e4a4b0ed9ee7f629d
Kevin Bost <kitokeboo@gmail.com>
2023-04-02 21:37:07 -0700

    Initial template test and it compiles.

---
+++ Thumbhash.Tests/ThumbHashTests.cs
@@ -0,0 +1,16 @@
+namespace Thumbhash.Tests;
+
+public class ThumbHashTests
+{
+    [Fact]
+    public void Method_WithPositiveValue_AddsOne()
+    {
+        //Arrange
+        AutoMocker mocker = new();
+
+        //Act
+
+        //Assert
+        //Assert.Equal(42, result);
+    }
+}
